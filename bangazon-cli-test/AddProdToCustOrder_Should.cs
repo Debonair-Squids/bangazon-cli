744a9f9c06e87c20d4c20f88443704433b3afa43
eagobert <eagobert@hotmail.com>
2018-02-07 15:29:41 -0600

    Initial commit

---
+++ bangazon-cli-test/AddProdToCustOrder_Should.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace bangazon_cli_test
+{
+    public class AddProdToCustOrder_Should
+    {
+
+        [Fact]
+        public void AddProdToCustOrder()
+        {
+            /*
+            -Given a customer has been made active in the program
+            -When the user selects the option to add a product to the active customer's order
+            -Then the user should be prompted with a numbered list of all products
+            -And when one is chosen, the product should be added to an order for the active customer*/
+            
+            Invoice newInvoice = new Invoice();
+            
+            newInvoice.OrderId = 1;
+            newInvoice.CustomerPaymentId = 1;
+            newInvoice.CustomerId = 1;
+            newInvoice.OrderDate = DateTime.Now;
+        }
+    }
+}
