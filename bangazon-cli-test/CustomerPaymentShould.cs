945b038e32be8d99a00feeedd1002cb65882b638
Jenna Solis <jennabsol@gmail.com>
2018-02-07 17:20:37 -0600

    just stuff

--- bangazon-cli-test/CustomerPaymentShould.cs
+++ bangazon-cli-test/CustomerPaymentShould.cs
@@ -1,45 +1,45 @@
-using System;
-using Xunit;
-using bangazon_cli;
+// using System;
+// using Xunit;
+// using bangazon_cli;
 
-namespace bangazon_cli_test
-{
-    public class CustomerPaymentShould
-    {
-        private Customer _customer;
-        public CustomerPaymentShould()
-        {
-            /*
-                Properties of Customer
-                    CustomerId = 1;
-                    FirstName = "Sofia";
-                    LastName= "Howard";
-                    StreetAddress = "110 Keoneula Blvd";
-                    State= "Hawaii";
-                    Zip = 96706;
-                    Phone= "808-729-4354";
-             */
-            _customer = new Customer()
-            {
-                CustomerId = 1,
-                FirstName = "Sofia",
-                LastName = "Howard",
-                StreetAddress = "110 Keoneula Blvd",
-                State = "Hawaii",
-                Zip = 96706,
-                Phone = 8087294354
-            };
-
-
+// namespace bangazon_cli_test
+// {
+//     public class CustomerPaymentShould
+//     {
+//         private Customer _customer;
+//         public CustomerPaymentShould()
+//         {
+//             /*
+//                 Properties of Customer
+//                     CustomerId = 1;
+//                     FirstName = "Sofia";
+//                     LastName= "Howard";
+//                     StreetAddress = "110 Keoneula Blvd";
+//                     State= "Hawaii";
+//                     Zip = 96706;
+//                     Phone= "808-729-4354";
+//              */
+//             _customer = new Customer()
+//             {
+//                 CustomerId = 1,
+//                 FirstName = "Sofia",
+//                 LastName = "Howard",
+//                 StreetAddress = "110 Keoneula Blvd",
+//                 State = "Hawaii",
+//                 Zip = 96706,
+//                 Phone = 8087294354
+//             };
 
 
-        }
-            [Fact]
-            public void AddNewPaymentType()
-            {
 
 
-            }
-    }
+//         }
+//             [Fact]
+//             public void AddNewPaymentType()
+//             {
 
-}
+
+//             }
+//     }
+
+// }
---
+++ bangazon-cli-test/PaymentShould.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using bangazon_cli;
+
+namespace bangazon_cli_test
+{
+    public class PaymentShould
+    {
+        // private Payment _Payment;
+        // public PaymentShould()
+        // {
+        //     /*
+        //     Properties of Payment
+        //     - PaymentId
+        //     - PaymentType
+        //      */
+
+        //     _Payment = new Payment()
+        //     {
+        //         PaymentId = 1,
+        //         PaymentType = "Visa"
+        //     };
+
+        // }
+
+        [Fact]
+        public void AddPayment()
+        {
+            Payment newPayment = new Payment
+            (
+                1,
+                "Visa"
+            );
+            Assert.Equal(newPayment.PaymentId, 1);
+            Assert.Equal(newPayment.PaymentType, "Visa");
+        }
+    }
+}
