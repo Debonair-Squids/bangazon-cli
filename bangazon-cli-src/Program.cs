2ad444ad2f15bfe3ebc648b00376ae66955e8fa6
JPage4 <jesse.page@me.com>
2018-02-07 15:32:44 -0600

    commit to make new branch

--- bangazon-cli-src/Program.cs
+++ bangazon-cli-src/Program.cs
@@ -6,7 +6,40 @@ namespace bangazon_cli
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int choice;
+
+            do
+            {
+                choice = MainMenu.Show();
+
+                switch (choice)
+                {
+                    case 1:
+                        // //add cohort
+                        // Console.WriteLine("Enter the Name of the Cohort (example: Day 22 or Evening 3)");
+                        // Console.Write(">");
+                        // string CohortName = Console.ReadLine();
+
+                        // Console.WriteLine("Enter the id of the technology used in this Cohort.");
+                        // //run code to display all tech in table with id number and tech name
+                        // Dictionary<int, string> techList = tech.GetTech();
+                        // foreach(KeyValuePair<int, string> t in techList){
+                        //     Console.WriteLine(t.Value + " = " + t.Key);
+                        // }
+
+                        // Console.Write(">");
+                        // int CohortTech = int.Parse(Console.ReadLine());
+
+                        // // Insert Cohort into database
+                        // db.Insert($@"
+                        //     INSERT INTO Cohort
+                        //     (CohortId, Name, TechId)
+                        //     VALUES
+                        //     (null, '{CohortName}', {CohortTech})
+                        // ");
+                        break;
+                }
+            }
         }
     }
 }
