109e35b66d280dcdbd3a715046e86a3349454c22
JPage4 <jesse.page@me.com>
2018-02-07 11:21:31 -0600

    add constructor methods to customer model

--- bangazon-cli-src/Models/Customer.cs
+++ bangazon-cli-src/Models/Customer.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace bangazon_cli
 {
-    class Customer
+    public class Customer
     {
         [Key]
         public int CustomerId {get; set;}
@@ -20,6 +23,31 @@ namespace bangazon_cli
         [Required]
         public int Zip {get; set;}
         [Required]
-        public int Phone {get; set;}
+        public long Phone {get; set;}
+
+        public Customer ()
+        {
+            this.CustomerId = 1;
+            this.FirstName = "John";
+            this.LastName = "Buttz";
+            this.StreetAddress = "123 Blah rd";
+            this.City = "Nashville";
+            this.State = "Tn";
+            this.Zip = 1;
+            this.Phone = 1;
+        }
+
+        // Constructor for Customer accepting all property parameters
+        public Customer (int cid, string first, string last, string address, string city, string state, int zip, int phone)
+        {
+            this.CustomerId = cid;
+            this.FirstName = first;
+            this.LastName = last;
+            this.StreetAddress = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.Phone = phone;
+        }
     }
 }
