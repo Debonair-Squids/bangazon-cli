055f1e249667119a9f5ce2667767b3a6ca331645
Kolden Prue <k.prue@yahoo.com>
2018-02-12 09:53:31 -0600

    Setting databaseInitializer to hold SQL methods , a few tests complete from ProductManagerShould

--- bangazon-cli-src/Managers/ProductManager.cs
+++ bangazon-cli-src/Managers/ProductManager.cs
@@ -6,16 +6,28 @@ namespace bangazon_cli
   public class ProductManager
   {
     private List<Product> products = new List<Product>();
-    CustomerManager cm = new CustomerManager();
+    // CustomerManager cm = new CustomerManager();
+    private DatabaseInitializer _db;
 
-    public void AddProduct(Product product)
+    public ProductManager(DatabaseInitializer db)
     {
-      products.Add(product);
+      _db = db;
     }
 
-    public void RemoveProduct(Product product)
+    //Passing the product info, and then the activeCustomer's id , so the product is created for the active customer
+    public int AddProductToSell(Product product)
+    {
+      products.Add(product);
+      // int id = _db.Insert($"insert into Product values (null, '{product.Title}', '{product.Description}', '{product.Price}', '{product.Quantity}', '{product.Category}', '{product.CustomerId}', '{product.DateCreated}') ");
+      return 7;
+    }
+
+    public string RemoveProduct(Product product)
     {
       products.Remove(product);
+
+      string confirmation = _db.Delete($"Delete from Product Where ProductId == {product.ProductId}");
+      return confirmation;
     }
     public List<Product> GetAllProducts()
     {
@@ -24,37 +36,43 @@ namespace bangazon_cli
 
     public IEnumerable<Product> GetAllCustomerProducts(Customer activeCustomer)
     {
-     IEnumerable<Product> allProducts = 
-     from prod in products
-     where prod.CustomerId == activeCustomer.CustomerId
-     select prod;
-     return allProducts; 
+      IEnumerable<Product> allProducts =
+      from prod in products
+      where prod.CustomerId == activeCustomer.CustomerId
+      select prod;
+      return allProducts;
 
     }
-    public Product GetSingleCustomerProduct(Customer activeCustomer , string title)
+    public Product GetSingleCustomerProduct(Customer activeCustomer, string title)
     {
       return products.Where(x => x.CustomerId == activeCustomer.CustomerId && x.Title == title).Single();
     }
 
 
-    public void UpdateProduct(Product product, int replaceChoice, string update)
+    public string UpdateProduct(Product product, int replaceChoice, string update)
     {
 
       switch (replaceChoice)
       {
         case 1:
           product.Title = update;
-          break;
+          return _db.Update($"UPDATE Product SET Title = '{update}' WHERE ProductId == {product.ProductId}");
         case 2:
           product.Description = update;
-          break;
+          return _db.Update($"UPDATE Product SET Description = '{update}' WHERE ProductId == {product.ProductId}");
         case 3:
-          product.Price = double.Parse(update);
-          break;
+          double parsedPatch = double.Parse(update);
+          product.Price = parsedPatch;
+          return _db.Update($"UPDATE Product SET Price = '{parsedPatch}' WHERE ProductId == {product.ProductId}");
         case 4:
-          product.Quantity = int.Parse(update);
-          break;
+          int parsedPatchQ = int.Parse(update);
+          product.Quantity = parsedPatchQ;
+          return _db.Update($"UPDATE Product SET Quantity = '{parsedPatchQ}' WHERE ProductId == {product.ProductId}");
       }
+      return "Not Updated";
     }
   }
 }
+// UPDATE table_name
+// SET column1 = value1, column2 = value2, ...
+// WHERE condition;
---
+++ queries.sql
@@ -0,0 +1,80 @@
+Delete from LineItem;
+Delete from Invoice;
+Delete from CustomerPayment;
+Delete from ProductCategory;
+Delete from Product;
+Delete from Customer;
+
+
+
+DROP TABLE LineItem;
+DROP TABLE Invoice;
+DROP TABLE CustomerPayment;
+DROP TABLE ProductCategory;
+DROP TABLE Product;
+DROP TABLE Customer;
+
+
+
+CREATE TABLE `LineItem` (
+`LineItemId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`InvoiceId` INTEGER NOT NULL, 
+`ProductId` INTEGER NOT NULL,
+FOREIGN KEY (`InvoiceId`) REFERENCES `Invoice`(`InvoiceId`),
+FOREIGN KEY (`ProductId`) REFERENCES `Product`(`ProductId`)
+
+);
+
+
+CREATE TABLE `Invoice` (
+`InvoiceId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`CustomerId` INTEGER NOT NULL, 
+`CustomerPaymentId` INTEGER NOT NULL,
+`InvoiceDate`  TEXT NOT NULL,
+FOREIGN KEY (`CustomerId`) REFERENCES `Customer`(`CustomerId`),
+FOREIGN KEY (`CustomerPaymentId`) REFERENCES `CustomerPayment`(`CustomerPaymentId`)
+
+);
+
+CREATE TABLE `CustomerPayment` (
+`CustomerPaymentId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`CustomerId` INTEGER NOT NULL, 
+`Account` INTEGER NOT NULL,
+`PaymentTypeName`  TEXT NOT NULL,
+FOREIGN KEY (`CustomerId`) REFERENCES `Customer`(`CustomerId`)
+);
+
+CREATE TABLE `ProductCategory` (
+`ProductCategoryId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`CategoryName` INTEGER NOT NULL
+);
+
+
+CREATE TABLE `Product` (
+`ProductId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`Title` TEXT NOT NULL, 
+`Description` TEXT NOT NULL,
+`Price`   INTEGER NOT NULL,
+`Quantity`   INTEGER NOT NULL,
+`ProductCategoryId` INTEGER NOT NULL,
+`CustomerId` INTEGER NOT NULL,
+`DateCreated`   TEXT NOT NULL,
+FOREIGN KEY (`CustomerId`) REFERENCES `Customer`(`CustomerId`),
+FOREIGN KEY (`ProductCategoryId`) REFERENCES `ProductCategory`(`ProductCategoryId`)
+
+);
+
+
+CREATE TABLE `Customer` (
+`CustomerId` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+`FirstName` TEXT NOT NULL, 
+`LastName` TEXT NOT NULL,
+`StreetAddress`   TEXT NOT NULL,
+`City`   TEXT NOT NULL,
+`'State'` TEXT NOT NULL,
+`Zip` TEXT NOT NULL,
+`Phone` TEXT NOT NULL
+);
+
+
+--Insert into productcategory & customer before executed queries for test
