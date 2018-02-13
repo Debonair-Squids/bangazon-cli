d6fa566a20224506f78353cc87fe7dd4ee41a7a7
Kolden Prue <k.prue@yahoo.com>
2018-02-12 22:31:04 -0600

    Modified ProductManager, added query statements to seed the db

--- bangazon-cli-src/Models/Product.cs
+++ bangazon-cli-src/Models/Product.cs
@@ -7,43 +7,24 @@ namespace bangazon_cli
   {
     [Key]
     public int ProductId { get; set; }
-    [Required]
     public string Title { get; set; }
-    [Required]
     public string Description { get; set; }
-    [Required]
     public double Price { get; set; }
-    [Required]
     public int Quantity { get; set; }
-    [Required]
     public int Category { get; set; }
-    [Required]
     public int CustomerId { get; set; }
-    [Required]
     public DateTime DateCreated { get; set; }
 
-    public Product()
-    {
-      this.DateCreated = DateTime.Now;
-      this.CustomerId = 1;
-      this.Title = "Kite";
-      this.Description = "Flies around";
-      this.Price = 9.99;
-      this.Quantity = 5;
-      this.Category = 1;
-      this.ProductId = 1;
-    }
-    
-    public Product(int productId, string title, string description, double price, int quantity, int category, int customerId)
-    {
-      this.DateCreated = DateTime.Now;
-      this.CustomerId = customerId;
-      this.ProductId = productId;
-      this.Description = description;
-      this.Price = price;
-      this.Title = title;
-      this.Quantity = quantity;
-      this.Category = category;
-    }
+    // public Product(string title, string description, double price, int quantity, int category, int customerId)
+    // {
+    //   this.ProductId = 0;
+    //   this.DateCreated = DateTime.Now;
+    //   this.CustomerId = customerId;
+    //   this.Description = description;
+    //   this.Price = price;
+    //   this.Title = title;
+    //   this.Quantity = quantity;
+    //   this.Category = category;
+    // }
   }
 }
